25357c6772bbb6a02137024e2783ef2b7026cef1
Devmolasses <devmolasses@gmail.com>
2017-06-02 12:08:14 -0500

    Impoved logic to look for rising, then falling edge.

--- FumeHoodWatchDog/FumeHoodWatchDog.ino
+++ FumeHoodWatchDog/FumeHoodWatchDog.ino
@@ -1,16 +1,18 @@
 // This is a watchdog program to be run on an Adafruit Trinket
 
-#define watchDogPin 1
+#define watchDogPin 13
 #define resetPin 2
 
-unsigned long watchDogInterval = 7000;
+unsigned long watchDogInterval = 30000; // Needs to see a pulse at lease every 30 seconds
 unsigned long resetDuration = 5000;
 unsigned long fumeHoodMonitorBootTime = 15000;
 unsigned long watchDogTimer;
 
+bool watchDogIsBeingPet = false;
 bool watchDogHasBeenPet = false;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(watchDogPin, INPUT);
   pinMode(resetPin, OUTPUT);
   digitalWrite(resetPin, HIGH);
@@ -19,9 +21,15 @@ void setup() {
 }
 
 void loop(){
-  watchDogHasBeenPet = digitalRead(watchDogPin) == 1;
-  if (watchDogHasBeenPet) watchDogTimer = millis();
+  watchDogIsBeingPet = digitalRead(watchDogPin) == 1;
+  if (watchDogIsBeingPet) {
+    watchDogHasBeenPet = true;
+  } else if (!watchDogIsBeingPet && watchDogHasBeenPet) {
+    watchDogTimer = millis();
+    watchDogHasBeenPet = false;
+  } 
   unsigned long now = millis();
+  Serial.println(now - watchDogTimer);
   if(now - watchDogTimer >= watchDogInterval){
     digitalWrite(resetPin, LOW);
     delay(resetDuration);

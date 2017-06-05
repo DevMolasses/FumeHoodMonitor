4db687890e352c806b4600581c0761f2c66147de
Devmolasses <devmolasses@gmail.com>
2017-06-05 14:48:31 -0500

    Added functions to simplify FumeHoodMonitor sketch

--- FumeHoodMonitor/ThingSpeak.ino
+++ FumeHoodMonitor/ThingSpeak.ino
@@ -4,15 +4,36 @@
 unsigned long ThingSpeakChannelID = CHANNELID;
 const char * ThingSpeakWriteAPIKey = WRITEAPIKEY;
 
-
+DateTime ThingSpeakTimer;
+const unsigned long thingSpeakPostingInterval = 30; // seconds between ThingSpeak posts
 
 void initializeThingSpeak() {
+  writeLCD("Starting ThingSpeak...");
   Serial.println("Starting ThingSpeak client");
   ThingSpeak.begin(client); // Start the ThingSpeak connection
+  ThingSpeakTimer = getRTCCurrentTime();
 }
 
 void postToThingSpeak(){
   ThingSpeak.writeFields(ThingSpeakChannelID, ThingSpeakWriteAPIKey);
+}
+
+void postDataToThingSpeak(float airTemp, float oilTemp, String flameStatus){
+  DateTime now = getRTCCurrentTime();
+  unsigned long timeSpan = now.unixtime() - ThingSpeakTimer.unixtime();
+  if (timeSpan >= thingSpeakPostingInterval) {
+    setThingSpeakField(1, airTemp);
+    setThingSpeakField(2,oilTemp);
+    setThingSpeakField(3,flameStatus);
+    setThingSpeakStatus(flameStatus);
+    postToThingSpeak();
+    ThingSpeakTimer = now;
+  }
+}
+
+void postThingSpeakStatus(String status){
+  setThingSpeakStatus(status);
+  postToThingSpeak();
 }
 
 void setThingSpeakStatus(String status){

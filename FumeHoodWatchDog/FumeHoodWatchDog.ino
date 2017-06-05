e9d30c91c6ecfa515dd5719f2c58f045ce5cc3fa
Devmolasses <devmolasses@gmail.com>
2017-06-05 14:44:54 -0500

    Changed to using interrupts to try to stop false resets.

--- FumeHoodWatchDog/FumeHoodWatchDog.ino
+++ FumeHoodWatchDog/FumeHoodWatchDog.ino
@@ -1,32 +1,36 @@
 // This is a watchdog program to be run on an Adafruit Trinket
 
-#define watchDogPin 1
-#define resetPin 2
+#define watchDogPin 2
+#define resetPin 3
+#define hbPin 1
 
 unsigned long watchDogInterval = 30000; // Needs to see a pulse at lease every 30 seconds
 unsigned long resetDuration = 5000;
 unsigned long fumeHoodMonitorBootTime = 15000;
-unsigned long watchDogTimer;
-
-bool watchDogIsBeingPet = false;
-bool watchDogHasBeenPet = false;
+volatile unsigned long watchDogTimer;
+volatile bool hbState = false;
+// bool watchDogIsBeingPet = false;
+// bool watchDogHasBeenPet = false;
 
 void setup() {
   pinMode(watchDogPin, INPUT);
+  attachInterrupt(0, petWatchDog, RISING);
   pinMode(resetPin, OUTPUT);
+  pinMode(hbPin, OUTPUT);
   digitalWrite(resetPin, HIGH);
+  digitalWrite(hbPin, hbState);
   delay(fumeHoodMonitorBootTime);
   watchDogTimer = millis();
 }
 
 void loop(){
-  watchDogIsBeingPet = digitalRead(watchDogPin) == 1;
-  if (watchDogIsBeingPet) {
-    watchDogHasBeenPet = true;
-  } else if (!watchDogIsBeingPet && watchDogHasBeenPet) {
-    watchDogTimer = millis();
-    watchDogHasBeenPet = false;
-  } 
+  // watchDogIsBeingPet = digitalRead(watchDogPin) == 1;
+  // if (watchDogIsBeingPet) {
+  //   watchDogHasBeenPet = true;
+  // } else if (!watchDogIsBeingPet && watchDogHasBeenPet) {
+  //   watchDogTimer = millis();
+  //   watchDogHasBeenPet = false;
+  // }
   unsigned long now = millis();
   if(now - watchDogTimer >= watchDogInterval){
     digitalWrite(resetPin, LOW);
@@ -35,4 +39,10 @@ void loop(){
     delay(fumeHoodMonitorBootTime);
     watchDogTimer = millis();
   }
+  digitalWrite(hbPin, hbState);
+}
+
+void petWatchDog(){
+  watchDogTimer = millis();
+  hbState = !hbState;
 }

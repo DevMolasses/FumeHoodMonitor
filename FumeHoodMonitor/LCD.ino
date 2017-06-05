75d565d2d07b96186eeb44dba9615e48f092ec3f
Devmolasses <devmolasses@gmail.com>
2017-06-05 14:52:22 -0500

    Updated initialization and added functionality

--- FumeHoodMonitor/LCD.ino
+++ FumeHoodMonitor/LCD.ino
@@ -1,8 +1,8 @@
 #include "Adafruit_LiquidCrystal.h"
 
-#define REDLITE A3
-#define GREENLITE A4
-#define BLUELITE A5
+#define REDLITE 5
+#define GREENLITE 6
+#define BLUELITE A3
 
 Adafruit_LiquidCrystal lcd(0);
 int brightness = 255;
@@ -13,12 +13,8 @@ void initializeLCD(){
   pinMode(REDLITE, OUTPUT);
   pinMode(GREENLITE, OUTPUT);
   pinMode(BLUELITE, OUTPUT);
-  setBacklight(255,0,0);
-  delay(1000);
-  setBacklight(0,0,255);
-  delay(1000);
-  setBacklight(255,255,0);
-  delay(1000);
+  cycleBacklightColor();
+  setBacklight("green");
 }
 
 void writeLCD(String message){
@@ -35,25 +31,22 @@ void writeLCD(String message1, String message2){
 
 void writeLCD(float ambientTemp, float fluidTemp, String flameStatus){
   lcd.setCursor(0,0);
-  lcd.print("Fume Hood Monitor   ");
+  lcd.print(make20Characters("Fume Hood Monitor"));
   lcd.setCursor(0,1);
-  lcd.print("Ambient Temp: ");
-  lcd.print(ambientTemp, 2);
+  lcd.print(make20Characters("Air Temp: " + String(ambientTemp, 2) + String((char)223) + "F"));
   lcd.setCursor(0,2);
-  lcd.print("Fluid Temp: ");
-  lcd.print(fluidTemp, 2);
+  lcd.print(make20Characters("Fluid Temp: " + String(fluidTemp, 2) + String((char)223) + "F"));
   lcd.setCursor(0,3);
-  lcd.print("Flame: ");
-  lcd.print(flameStatus);
+  lcd.print(make20Characters("Flame: " + flameStatus));
 }
 
 void writeLCD(String timeString, float ambientTemp, float fluidTemp, String flameStatus){
   lcd.setCursor(0,0);
   lcd.print(make20Characters(timeString));
   lcd.setCursor(0,1);
-  lcd.print(make20Characters("Ambient Temp: " + String(ambientTemp, 2)));
+  lcd.print(make20Characters("Air Temp: " + String(ambientTemp, 2) + String((char)223) + "F"));
   lcd.setCursor(0,2);
-  lcd.print(make20Characters("Fluid Temp: " + String(fluidTemp, 2)));
+  lcd.print(make20Characters("Fluid Temp: " + String(fluidTemp, 2) + String((char)223) + "F"));
   lcd.setCursor(0,3);
   lcd.print(make20Characters("Flame: " + flameStatus));
 }
@@ -79,4 +72,47 @@ void setBacklight(uint8_t r, uint8_t g, uint8_t b) {
   analogWrite(REDLITE, r);
   analogWrite(GREENLITE, g);
   analogWrite(BLUELITE, b);
+}
+
+void setBacklight(String color) {
+  uint8_t r, g, b;
+  if      (color == "red")    {r = 255; g = 0;   b = 0;}
+  else if (color == "orange") {r = 255; g = 50;  b = 0;}
+  else if (color == "yellow") {r = 255; g = 255; b = 0;}
+  else if (color == "green")  {r = 0;   g = 255; b = 0;}
+  else if (color == "blue")   {r = 0;   g = 0;   b = 255;}
+  else                        {r = 255; g = 255; b = 255;}
+  setBacklight(r, g, b);
+}
+
+void cycleBacklightColor() {
+  uint8_t d = 1;
+  for (uint8_t i = 0; i < 255; i++) {
+    setBacklight(0, i, 0);
+    delay(d);
+  }
+  for (uint8_t i = 0; i < 255; i++) {
+    setBacklight(0, 254, i);
+    delay(d);
+  }
+  for (uint8_t i = 0; i < 255; i++) {
+    setBacklight(0, 254 - i, 254);
+    delay(d);
+  }
+  for (uint8_t i = 0; i < 255; i++) {
+    setBacklight(i, 0, 254);
+    delay(d);
+  }
+  for (uint8_t i = 0; i < 255; i++) {
+    setBacklight(254, 0, 254 - i);
+    delay(d);
+  }
+  for (uint8_t i = 0; i < 255; i++) {
+    setBacklight(254, i, 0);
+    delay(d);
+  }
+  for (uint8_t i = 0; i < 255; i++) {
+    setBacklight(254 - i, 254, 0);
+    delay(d);
+  }
 }

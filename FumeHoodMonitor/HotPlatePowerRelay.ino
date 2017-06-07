b188152a136483c237a3915e88252958f50018cf
Devmolasses <devmolasses@gmail.com>
2017-06-07 16:57:49 -0500

    Updated control functions

--- FumeHoodMonitor/HotPlatePowerRelay.ino
+++ FumeHoodMonitor/HotPlatePowerRelay.ino
@@ -1,15 +1,32 @@
 #define relayPin 18
 
+bool onFire = false;
+bool airHigh = false;
+bool oilHigh = false;
+
 void initializePowerRelay(){
   writeLCD("Starting PWR Relay..");
+  Serial.println("Starting Hot Plate Power Relay");
   pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, LOW);
-}
-
-void powerUpHotPlate(){
   digitalWrite(relayPin, HIGH);
 }
 
-void powerDownHotPlate(){
+void powerUpHotPlate(){
   digitalWrite(relayPin, LOW);
+}
+
+void powerDownHotPlate(){
+  digitalWrite(relayPin, HIGH);
+}
+
+void controlHotPlatePowerRelay(float airTemp, float oilTemp){
+  if (isFlamePresent() || isAirTempOnFire(airTemp) || isOilTempOnFire(oilTemp)) onFire = true;
+  else {
+    if (isOilTempHigh(oilTemp)) oilHigh = true;
+    else if (isOilTempLow(oilTemp)) oilHigh = false;
+    if (isAirTempHigh(airTemp)) airHigh = true;
+    else if (isAirTempLow(airTemp)) airHigh = false;
+  }
+  if(onFire || oilHigh || airHigh) powerDownHotPlate();
+  else powerUpHotPlate();
 }

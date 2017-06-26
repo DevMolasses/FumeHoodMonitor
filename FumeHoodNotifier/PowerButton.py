7ef79116f0bdb6a460c2e88be0d285a6a162bc06
Devmolasses <devmolasses@gmail.com>
2017-06-26 09:26:59 -0500

    Added descriptive comments on how to use the script

--- FumeHoodNotifier/PowerButton.py
+++ FumeHoodNotifier/PowerButton.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+# Script to shudown RPi using a button
+# Wire the button to pull GPIO 3 (Pin 5) to Ground to shutdown
+# RPi has built-in function to wake RPi with the same button,
+# so the button will both shutdown the RPi and wake it back up
+
 import RPi.GPIO as GPIO
 import subprocess
 
